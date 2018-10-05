6e1843fc877be885f471d26d532217691203cb00
Joshua Wilson <jww23@gold04.cs.calvin.edu>
2018-10-05 18:48:29 -0400

    Lab05

---
+++ Lab05/app/src/main/java/edu/calvin/cs262/jww23/lab05/BookLoader.java
@@ -0,0 +1,26 @@
+package edu.calvin.cs262.jww23.lab05;
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+
+    private String mQueryString;
+
+
+    public BookLoader( Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+    }
+
+}
