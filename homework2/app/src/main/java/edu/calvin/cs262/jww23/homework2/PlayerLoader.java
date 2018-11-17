74f35ee8aa645b7c058e043a05ba7e87d90b13bd
jww23 <43280008+jww23@users.noreply.github.com>
2018-11-16 22:19:41 -0500

    Updating documentation

--- homework2/app/src/main/java/edu/calvin/cs262/jww23/homework2/PlayerLoader.java
+++ homework2/app/src/main/java/edu/calvin/cs262/jww23/homework2/PlayerLoader.java
@@ -2,6 +2,9 @@ package edu.calvin.cs262.jww23.homework2;
 
 import android.content.Context;
 import android.support.v4.content.AsyncTaskLoader;
+/**
+* PLayerLoader class allows the program to determine which player is being requested
+*/
 
 
 public class PlayerLoader extends AsyncTaskLoader<String> {
