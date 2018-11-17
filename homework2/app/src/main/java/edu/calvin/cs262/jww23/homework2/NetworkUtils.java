215a2bef6fc7f6cca354238dabcf4601a936ca09
jww23 <43280008+jww23@users.noreply.github.com>
2018-11-16 22:24:37 -0500

    Updating documentation

--- homework2/app/src/main/java/edu/calvin/cs262/jww23/homework2/NetworkUtils.java
+++ homework2/app/src/main/java/edu/calvin/cs262/jww23/homework2/NetworkUtils.java
@@ -8,6 +8,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+/**
+*The Utility class enables us to get information from the player web service
+*/
+
 
 public class NetworkUtils {
 
